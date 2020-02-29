3a185e84bc8e115a420180c371adfb25995d0e0f
timmymin <59441786+timmymin@users.noreply.github.com>
2020-02-29 22:51:11 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/1yvfpm3bqce1yvfpm3bqce.jpg');
+    } else {
+      myImage.setAttribute ('src','images/14237562_10154370379482850_8484062923204743435_n.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
